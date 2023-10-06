614795620556e8b410f2115d0cb41a019f86708e
Nondh Panithanpaisal <nonsk131@gmail.com>
2023-10-06 12:49:14 -0400

    adding legacy sets of isochrones for DECAM and LSST-DP0

--- src/IsochroneBase.cpp
+++ src/IsochroneBase.cpp
@@ -143,7 +143,7 @@ void IsochroneBase:: readIsocrhones(const string& inputDir,const string& dirname
 	vector<double> fe(fe1,fe1+sizeof(fe1)/sizeof(fe1[0]));
 	for(size_t i=0;i<fe.size();++i)
 	{
-		if((photoSys.compare("WFIRST-LSST")==0)||(photoSys.compare("WFIRST")==0)||(photoSys.compare("WFIRST-HST")==0)||(photoSys.compare("GAIA")==0)||(photoSys.compare("GAIADR2")==0)) {
+		if((photoSys.compare("WFIRST-LSST")==0)||(photoSys.compare("WFIRST")==0)||(photoSys.compare("WFIRST-HST")==0)||(photoSys.compare("GAIA")==0)||(photoSys.compare("GAIADR2")==0)||(photoSys.compare("DECAM")==0)||(photoSys.compare("LSST-DP0")==0)) {
 			//cout<<"Using new Zsun";
 			FeH.push_back(log10(fe[i]/0.0152));
 		}
@@ -224,7 +224,7 @@ int IsochroneBase:: readfile(const string& fname,double alpha1,double feH1,const
 	Isochrone icData;
 	icData.age=-1.0;
 
-	if((iso_fileinfo.photoSysName.compare("WFIRST-LSST")==0)||(iso_fileinfo.photoSysName.compare("WFIRST")==0)||(iso_fileinfo.photoSysName.compare("WFIRST-HST")==0)||(iso_fileinfo.photoSysName.compare("WFIRST-LST")==0)||(iso_fileinfo.photoSysName.compare("GAIA")==0)||(iso_fileinfo.photoSysName.compare("GAIADR2")==0)) { 
+	if((iso_fileinfo.photoSysName.compare("WFIRST-LSST")==0)||(iso_fileinfo.photoSysName.compare("WFIRST")==0)||(iso_fileinfo.photoSysName.compare("WFIRST-HST")==0)||(iso_fileinfo.photoSysName.compare("WFIRST-LST")==0)||(iso_fileinfo.photoSysName.compare("GAIA")==0)||(iso_fileinfo.photoSysName.compare("GAIADR2")==0)||(iso_fileinfo.photoSysName.compare("DECAM")==0)||(iso_fileinfo.photoSysName.compare("LSST-DP0")==0)) { 
 		//cout<<"Using new Zsun";
 		icData.FeH=log10(feH1/0.0152);
 	}
@@ -235,7 +235,7 @@ int IsochroneBase:: readfile(const string& fname,double alpha1,double feH1,const
 	icData.addDwarfs(int(iso_fileinfo.extraid.size()),dwarfOn);
 
 
-	//cout<<"magids are "<<iso_fileinfo.magid[0]<<" "<<iso_fileinfo.magid[1]<<" "<<iso_fileinfo.magid[2]<<"\n";
+	cout<<"magids are "<<iso_fileinfo.magid[0]<<" "<<iso_fileinfo.magid[1]<<" "<<iso_fileinfo.magid[2]<<"\n";
 
 	int j,i=0;
 	int iage;
@@ -252,26 +252,38 @@ int IsochroneBase:: readfile(const string& fname,double alpha1,double feH1,const
 			    continue;
 			}
 			if(iso_fileinfo.photoSysName.compare("WFIRST")==0){
-				//cout<<"using new WFIRST Isochrones"<<endl;
+				cout<<"using new WFIRST Isochrones"<<endl;
 				iage=1;
 				linage=1;
 				sscanf(buf,"%f%G%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f",&x[0],&x[1],&x[2],&x[3],&x[4],&x[5],&x[6],&x[7],&x[8],&x[9],&x[10],&x[11],&x[12],&x[13],&x[14],&x[15],&x[16],&x[17],&x[18],&x[19],&x[20],&x[21],&x[22],&x[23],&x[24],&x[25],&x[26],&x[27],&x[28],&x[29]);
 				//cout<<x[0]<<"  "<<x[1]<<"  "<<x[2]<<endl;
 			}
 			else if(iso_fileinfo.photoSysName.compare("WFIRST-HST")==0){
-				//cout<<"using WFIRST-HST Isochrones"<<endl;
+				cout<<"using WFIRST-HST Isochrones"<<endl;
 				iage=1;
 				linage=0;
 				sscanf(buf,"%f%G%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f",&x[0],&x[1],&x[2],&x[3],&x[4],&x[5],&x[6],&x[7],&x[8],&x[9],&x[10],&x[11],&x[12],&x[13],&x[14],&x[15],&x[16],&x[17],&x[18],&x[19],&x[20],&x[21],&x[22],&x[23],&x[24],&x[25],&x[26]);
 			}
+			else if(iso_fileinfo.photoSysName.compare("DECAM")==0){
+				cout<<"using DECAM Isochrones"<<endl;
+				iage=2;
+				linage=0;
+				sscanf(buf,"%f%f%G%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%e%f%e%e%e%e%e%f%e%f%f%f%f%f%f%f",&x[0],&x[1],&x[2],&x[3],&x[4],&x[5],&x[6],&x[7],&x[8],&x[9],&x[10],&x[11],&x[12],&x[13],&x[14],&x[15],&x[16],&x[17],&x[18],&x[19],&x[20],&x[21],&x[22],&x[23],&x[24],&x[25],&x[26],&x[27],&x[28],&x[29],&x[30],&x[31],&x[32],&x[33]);
+			}
+			else if(iso_fileinfo.photoSysName.compare("LSST-DP0")==0){
+				cout<<"using LSST-DP0 Isochrones"<<endl;
+				iage=2;
+				linage=0;
+				sscanf(buf,"%f%f%G%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%e%f%e%e%e%e%e%f%e%f%f%f%f%f%f%f",&x[0],&x[1],&x[2],&x[3],&x[4],&x[5],&x[6],&x[7],&x[8],&x[9],&x[10],&x[11],&x[12],&x[13],&x[14],&x[15],&x[16],&x[17],&x[18],&x[19],&x[20],&x[21],&x[22],&x[23],&x[24],&x[25],&x[26],&x[27],&x[28],&x[29],&x[30],&x[31],&x[32],&x[33]);
+			}
 			else if(iso_fileinfo.photoSysName.compare("GAIA")==0){
-				//cout<<"using Gaia Isochrones"<<endl;
+				cout<<"using Gaia Isochrones"<<endl;
 				iage=1;
 				linage=1;
 				sscanf(buf,"%f%G%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f",&x[0],&x[1],&x[2],&x[3],&x[4],&x[5],&x[6],&x[7],&x[8],&x[9],&x[10],&x[11],&x[12],&x[13],&x[14],&x[15],&x[16],&x[17],&x[18],&x[19],&x[20],&x[21],&x[22],&x[23],&x[24],&x[25]);
 			}
 			else if(iso_fileinfo.photoSysName.compare("GAIADR2")==0){
-				//cout<<"using Gaia Isochrones"<<endl;
+				cout<<"using Gaia Isochrones"<<endl;
 				iage=1;
 				linage=1;
 				sscanf(buf,"%f%G%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f",&x[0],&x[1],&x[2],&x[3],&x[4],&x[5],&x[6],&x[7],&x[8],&x[9],&x[10],&x[11],&x[12],&x[13],&x[14],&x[15],&x[16],&x[17],&x[18],&x[19],&x[20],&x[21],&x[22],&x[23],&x[24],&x[25]);
@@ -281,7 +293,7 @@ int IsochroneBase:: readfile(const string& fname,double alpha1,double feH1,const
 				sscanf(buf,"%G%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f",&x[0],&x[1],&x[2],&x[3],&x[4],&x[5],&x[6],&x[7],&x[8],&x[9],&x[10],&x[11],&x[12],&x[13],&x[14],&x[15]);
 			}
 
-			//cout<<"iage "<<iage<<" linage "<<linage<<endl;
+			cout<<"iage "<<iage<<" linage "<<linage<<endl;
 			sprintf(buf_check,"%f ",x[2]);
 			if(strncmp(buf_check,"nan",3)==0)
 				continue;
@@ -295,7 +307,7 @@ int IsochroneBase:: readfile(const string& fname,double alpha1,double feH1,const
 			if(double(x[iage])!=icData.age)
 			{
 				icData.age=x[iage];
-				//cout<<"Finished with age "<<x[iage]<<" index "<<iage<<endl;
+				cout<<"Finished with age "<<x[iage]<<" index "<<iage<<endl;
 				icv.push_back(icData);
 				i++;
 			}
@@ -306,6 +318,13 @@ int IsochroneBase:: readfile(const string& fname,double alpha1,double feH1,const
 				icv.back().Lum.push_back(x[4]);
 				icv.back().Teff.push_back(x[5]);
 				icv.back().Grav.push_back(x[6]);
+			}
+			else if ((iso_fileinfo.photoSysName.compare("DECAM")==0)||(iso_fileinfo.photoSysName.compare("LSST-DP0")==0)) {
+				icv.back().m.push_back(x[3]);
+				icv.back().Mact.push_back(x[5]); // M_act does not exist in the new isochrone
+				icv.back().Lum.push_back(x[6]);
+				icv.back().Teff.push_back(x[7]);
+				icv.back().Grav.push_back(x[8]);
 			}
 			else {
 				icv.back().m.push_back(x[1]);
